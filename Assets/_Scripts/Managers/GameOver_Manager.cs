5be4c26d11a73ce451f8ac4d159e12cee349ef25
Deive_Ex <feliperobos@gmail.com>
2019-10-04 21:19:32 -0300

    Fiz o GET do Rank do leaderboar

--- Assets/_Scripts/Managers/GameOver_Manager.cs
+++ Assets/_Scripts/Managers/GameOver_Manager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameOver_Manager : MonoBehaviour {
 	public TextMeshProUGUI gameOverText;
 	public string sceneToReturn;
+	[BoxTitle("Leaderboard")]
+	public string baseURL;
 	public GameObject leaderboardPanel;
 	public TMP_Text topPlayers;
 	public TMP_Text playerPoints;
@@ -14,25 +17,52 @@ public class GameOver_Manager : MonoBehaviour {
 	[TextArea]
 	public string[] gameOverTexts;
 
+	private string[] ranks;
+
 	private void OnEnable() {
 		leaderboardPanel.SetActive(false);
-		gameOverText.text = gameOverTexts[Random.Range(0, gameOverTexts.Length)];
+		gameOverText.text = gameOverTexts[UnityEngine.Random.Range(0, gameOverTexts.Length)];
 		StartCoroutine(ShowLeaderboards());
 	}
 
 	private IEnumerator ShowLeaderboards() {
+		ranks = null;
+
 		yield return new WaitForSeconds(2);
+
+		playerPoints.text = "Sua pontuação: " + Game_Manager.Instance.GetPoints();
+		topPlayers.text = "Carregando...";
 
 		leaderboardPanel.SetActive(true);
 
-		//TODO Fazer requests
+		UriBuilder uri = new UriBuilder(baseURL + "/getRank.php");
+		yield return StartCoroutine(Request_Manager.Instance.GET_Routine(uri, getLeaderBoard_Callback));
 
-		topPlayers.text = "Não foi possível acessar o servidor";
-		playerPoints.text = "Sua pontuação: " + Game_Manager.Instance.GetPoints();
+		if(ranks == null) {
+			yield break;
+		}
+
+		topPlayers.text = string.Empty;
+		for (int i = 0; i < ranks.Length; i++) {
+			yield return new WaitForSeconds(0.5f);
+			topPlayers.text += ranks[i] + "\n";
+		}
 	}
 
 	public void ResetGame() {
 		Game_Manager.Instance.ResetPoints();
 		SceneManager.LoadScene(sceneToReturn);
+	}
+
+	private void getLeaderBoard_Callback(string response) {
+		Debug.Log(response);
+
+		if (string.IsNullOrEmpty(response)) {
+			Debug.LogError("Error getting ranks");
+			topPlayers.text = "Não foi possível acessar o servidor";
+			return;
+		}
+
+		ranks = response.Split('\n');
 	}
 }
